ad50152fb8697a4fb72d94eda20eaed4f9c0b4fe
jlmoy <jlmoy@unix2.csc.calpoly.edu>
2018-10-15 21:45:23 -0700

    update Students

--- datasets/STUDENT/STUDENT-cleanup.sql
+++
@@ -1,7 +0,0 @@
--- Christopher Creber
--- ccreber@calpoly.edu
-
--- STUDENT CLEANUP
-
-DROP TABLE teachers;
-DROP TABLE students;
--- datasets/STUDENT/STUDENT-reset.sql
+++
@@ -1,5 +0,0 @@
-source STUDENT/STUDENT-cleanup.sql
-source STUDENT/STUDENT-setup.sql
-source STUDENT/STUDENT-build-teachers.sql
-source STUDENT/STUDENT-build-list.sql
-source STUDENT/STUDENTS-modify.sql
