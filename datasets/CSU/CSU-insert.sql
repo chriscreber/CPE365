da681af26160e8eba5b7cd58335642bfda4eb08c
Unknown <clcreber1996@gmail.com>
2018-10-29 22:54:26 -0700

    Auto stash before merge of "master" and "origin/master"

--- datasets/CSU/CSU-insert.sql
+++ datasets/CSU/CSU-insert.sql
@@ -1,4 +1,12 @@
 source CSU-build-Campuses.sql
+source CSU-build-degrees.sql
+source CSU-build-disciplines.sql
+source CSU-build-discipline-enrollments.sql
+source CSU-build-enrollments.sql
+source CSU-build-csu-fees.sql
+source CSU-build-faculty.sql
+
+source CSU-build-Campuses.sql
 source CSU-build-csu-fees.sql
 source CSU-build-degrees.sql
 source CSU-build-disciplines.sql
